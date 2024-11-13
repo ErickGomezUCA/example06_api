86dc04affd6dc3b3068a456268299ba1894d5e19
ErickGomezUCA <00300723@uca.edu.sv>
2024-11-13 11:13:50 -0600

    Add API files

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+export default {
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testMatch: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  transform: {
+    // Use Babel to transform .js files
+    '^.+\\.js$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js', 'json', 'node'],
+};
